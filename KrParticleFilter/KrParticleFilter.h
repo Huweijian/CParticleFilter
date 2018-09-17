bbdb4940dd0e44d3b37da54a4c60a150ba5710e3
胡伟健 <weijian.hu@krvision.cn>
2018-09-17 21:53:32 +0800

    Resolve "粒子滤波框架"

--- KrParticleFilter/KrParticleFilter.h
+++ KrParticleFilter/KrParticleFilter.h
@@ -3,17 +3,24 @@
 #include <array>
 #include <string>
 #include <Eigen\core>
-
+#include <ctime>
 typedef Eigen::VectorXd Particle;
 
 class ParticleFilter {
 public:
-	ParticleFilter() {};
+	enum ResampleTriggeredMethod {
+		RATIO,  // Trigger resampling if ratio of effective to total particles falls below threshold.
+		INTERVAL // Trigger resampling if our interval counter is an integer multiple of the desired SamplingInterval
+	};
+
+	ParticleFilter() {
+		std::srand((unsigned int)time(0));
+	};
 	~ParticleFilter() {};
 	unsigned int numStateVariables;
 	unsigned int numParticles;
-	std::function<void(Eigen::MatrixXd, Eigen::VectorXd)> stateTransitionFcn;
-	std::function<void(Eigen::MatrixXd, Eigen::VectorXd)> measurementLikelihoodFcn;
+	std::function<void(Eigen::MatrixXd&, const Eigen::VectorXd&)> stateTransitionFcn;
+	std::function<Eigen::VectorXd (Eigen::MatrixXd, const Eigen::VectorXd&)> measurementLikelihoodFcn;
 	std::vector<bool> isStateVariableCircular;
 	std::string resamplingMethod = "multinominal";
 	std::string stateEstimationMethod = "maxweight";
@@ -21,17 +28,31 @@ public:
 	Eigen::VectorXd weights;
 	Particle state;
 	Eigen::Matrix2d stateCovariance;
-
-	// TODO:
-	bool resamplingPolicy = false;
-
 	void initilize(int numParticles, Eigen::VectorXd mean, Eigen::MatrixXd covariance, std::vector<bool> circularVariables = std::vector<bool>());
+	void predict(const Eigen::VectorXd &trans);
+	void predict(const Eigen::VectorXd &trans, Eigen::VectorXd &stateOut , Eigen::MatrixXd &covOut);
+	void correct(const Eigen::VectorXd &measurement, Eigen::VectorXd &stateOut, Eigen::MatrixXd &covOut);
 
-
-
-
+	// TODO: 重采样策略封装成类(Matlab robotics.ResamplingPolicy)
+	bool resamplingPolicy = false;
+	ResampleTriggeredMethod triggeredMethod = RATIO;
+	unsigned int samplingInterval = 1;
+	double minEffectiveParticleRatio = 0.5;
+	
 private:
+	bool isResamplingTriggered();
+	void resample();
+	Eigen::VectorXi multinominalResampler();
+	Eigen::VectorXi findSortedSampleIndices(const Eigen::VectorXd & randSamples);
+
 	void sampleGaussian(Eigen::VectorXd & initialMean, Eigen::MatrixXd & initialCov, std::vector<bool>& isCircVar);
-	double normalSample(double dummy, double mean, double var);
-	void randn(Eigen::MatrixXd & mat, int row, int col);
+	Eigen::MatrixXd randn(int row, int col);
+	void getStateEstimate(Eigen::VectorXd &stateOut, Eigen::MatrixXd &covOut);
+	void meanStateEstimator(Eigen::VectorXd &stateOut, Eigen::MatrixXd &covOut);
+	void nonCircleFitToSamples(Eigen::VectorXd &sampleMean, Eigen::MatrixXd & sampleCov);
+
+	int intervalCounter = 0;
+
+	const double epsilon = 1e-6;
+
 };
